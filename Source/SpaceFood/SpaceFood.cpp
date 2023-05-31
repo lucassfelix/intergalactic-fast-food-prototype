d88a6b3598cecd432f1d7d2d8d6075077727a5b1
Lucas Félix <lucas.felix.gamedev@gmail.com>
2023-05-31 13:51:44 -0300

    Project Setup

---
+++ Source/SpaceFood/SpaceFood.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceFood.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceFood, "SpaceFood" );
